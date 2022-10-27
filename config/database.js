a402d4d89e58312413ccda315a8d7952b10b5386
Carter Pierre <carterspierre@gmail.com>
2022-10-27 18:17:52 -0400

    first commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demo'
+    'url': 'mongodb+srv://cometcarter:Catsnapsbats@cluster0.seiom8q.mongodb.net/main?retryWrites=true&w=majority',
+    'dbName': 'main'
 };
